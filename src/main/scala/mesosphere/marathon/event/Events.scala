6e42de2b3801e913cd96f0268b99ae33cb7a830c
drexin <dario.rexin@r3-tech.de>
2014-10-30 00:48:57 +0100

    added first play-json formats and removed json4s

--- src/main/scala/mesosphere/marathon/event/Events.scala
+++ src/main/scala/mesosphere/marathon/event/Events.scala
@@ -113,7 +113,7 @@ case class HealthStatusChanged(
 
 // upgrade messages
 
-trait UpgradeEvent extends MarathonEvent
+sealed trait UpgradeEvent extends MarathonEvent
 
 case class GroupChangeSuccess(
   groupId: PathId,
